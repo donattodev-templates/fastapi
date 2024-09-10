6d1b0aa62e433b3f8d197317c2c54b136847511b
Vinicius Morgado <34577818+viniciusmorgado@users.noreply.github.com>
2024-09-09 21:26:49 -0300

    Add repository sample and basic error handling

--- pyservice/api/routes/submit_scores.py
+++ pyservice/api/routes/submit_scores.py
@@ -1,9 +1,13 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+
 from pyservice.domain.entities.score import Score
+from pyservice.infrastructure.repositories.add_score_repository import add_score_repository
 
 router = APIRouter()
 
 @router.post("/submit-score")
 def submit_score(score: Score):
-    print("Received!", score)
-    return "Hello, again"
+    try:
+        return add_score_repository(score.name, score.math_score, score.english_score)
+    except Exception as ex:
+        raise HTTPException(status_code=500, detail=f"Error: {ex}")
---
+++ pyservice/infrastructure/repositories/add_score_repository.py
@@ -0,0 +1,2 @@
+def add_score_repository(name: str, math_score: int, english_score: int):
+    return { "name": name, "math_score": math_score, "english_score": english_score }
