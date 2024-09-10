53ec24ff1bb3a8e631fdadff1e5c59657557da63
Vinicius Morgado <34577818+viniciusmorgado@users.noreply.github.com>
2024-09-10 01:36:52 -0300

    Implements the first version of Dockerfile, change psycopg2 to psycopg2-binary to fix build problems in Docker, add new TODO comment to postgres adapter

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,15 @@
-FROM ubuntu:latest
-LABEL authors="Vinicius Morgado"
+FROM python:3.12.4-alpine3.20
 
-ENTRYPOINT ["top", "-b"]
+WORKDIR /app
+COPY pyproject.toml poetry.lock ./
+
+RUN pip install --upgrade pip; \
+    pip install poetry;
+
+RUN poetry config virtualenvs.in-project true; \
+    poetry install --no-interaction --no-ansi;
+
+COPY pyservice /app/pyservice
+CMD ["poetry", "run", "uvicorn", "pyservice.api.main:app", "--port", "8000", "--reload"]
+
+EXPOSE 8000
--- pyservice/infrastructure/adapters/postgres_adapter.py
+++ pyservice/infrastructure/adapters/postgres_adapter.py
@@ -3,6 +3,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # TODO: Implements a interface for the adapter and test with MySQL
+# TODO: Move the connection string to a configuration file, check if is possible to re-use pyproject.toml
+# TODO: Create a development vs production (for the docker container) connection string
 
 SQLALCHEMY_DATABASE_URL = 'postgresql://d5uRf3S76W98prJF4cCKqw:P2uADnKtBYVkNpjZ68drFS@localhost/pyservice-db'
 
