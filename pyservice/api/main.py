45b9543a19c2b5a245a5dc71f186d1b0ea8dffda
Vinicius Morgado <34577818+viniciusmorgado@users.noreply.github.com>
2024-09-11 00:10:45 -0300

    Add logging exception for patch

--- pyservice/api/main.py
+++ pyservice/api/main.py
@@ -1,14 +1,17 @@
 from fastapi import FastAPI
+from logging import basicConfig, INFO
 from pyservice.api.routes.score_router import router
 from pyservice.infrastructure.mapping.score_table import create_score_table
 
 # TODO: Create a loop and cache for both bootstrap classes and routers
+
 
 def bootstrap_database():
     """Creates database tables schematics if they don't exist at application startup."""
     create_score_table()
 
 def bootstrap_application() -> FastAPI:
+    basicConfig(level=INFO, format="%(asctime)s - %(levelname)s - %(message)s")
     boot = FastAPI(title="PyService Template", description="This is a internal template for fastapi microservices")
     boot.include_router(router)
     return boot
--- pyservice/infrastructure/repositories/score/patch_score_repository.py
+++ pyservice/infrastructure/repositories/score/patch_score_repository.py
@@ -2,6 +2,7 @@ from sqlalchemy import update
 from uuid import UUID
 from pyservice.infrastructure.models.score_model import ScoreModel
 from sqlalchemy.orm import Session
+from logging import exception
 
 
 def patch_score_repository(db, score_id: UUID, name: str, math_score: int, english_score: int):
@@ -10,13 +11,16 @@ def patch_score_repository(db, score_id: UUID, name: str, math_score: int, engli
             row = db.query(ScoreModel).filter(ScoreModel.id == score_id).first()
 
             if row.name != name:
-                query = (update(ScoreModel).where(ScoreModel.id == score_id).values({'name': name}))
+                query = (update(ScoreModel).where(ScoreModel.id == score_id)
+                                           .values({'name': name}))
 
             if row.math_score != math_score:
-                query = (update(ScoreModel).where(ScoreModel.id == score_id).values({'math_score': math_score}))
+                query = (update(ScoreModel).where(ScoreModel.id == score_id)
+                                           .values({'math_score': math_score}))
 
             if row.english_score != english_score:
-                query = (update(ScoreModel).where(ScoreModel.id == score_id).values({'english_score': english_score}))
+                query = (update(ScoreModel).where(ScoreModel.id == score_id)
+                                           .values({'english_score': english_score}))
 
             db.execute(query)
             db.commit()
@@ -24,7 +28,7 @@ def patch_score_repository(db, score_id: UUID, name: str, math_score: int, engli
 
             return {"Message": "Updated successfully!"}
 
-        except Exception:
+        except Exception as ex:
             session.rollback()
-            # TODO: Handle the exception appropriately (log, return error message, etc.)
-            raise  # Re-raise the exception for now
+            # TODO: Log the errors to OpenTelemetry
+            exception(f"Error: {ex}")
