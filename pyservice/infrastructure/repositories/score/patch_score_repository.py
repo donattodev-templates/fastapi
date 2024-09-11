5120888e1561483b337d12c95e84a9189a990949
Vinicius Morgado <34577818+viniciusmorgado@users.noreply.github.com>
2024-09-10 23:28:29 -0300

    Implements a preliminary version of the patch repository using ACID principles

--- pyservice/infrastructure/repositories/score/patch_score_repository.py
+++ pyservice/infrastructure/repositories/score/patch_score_repository.py
@@ -1,27 +1,30 @@
-from typing import cast
-
 from sqlalchemy import update
-from uuid import uuid4
+from uuid import UUID
 from pyservice.infrastructure.models.score_model import ScoreModel
+from sqlalchemy.orm import Session
 
 
-def patch_score_repository(db, score_id: uuid4, name: str, math_score: int, english_score: int):
+def patch_score_repository(db, score_id: UUID, name: str, math_score: int, english_score: int):
+    with Session(db) as session:
+        try:
+            row = db.query(ScoreModel).filter(ScoreModel.id == score_id).first()
 
-    entry_to_be_updated = db.query(ScoreModel).filter(ScoreModel.id == score_id).first()
-    update_data = {}
+            if row.name != name:
+                query = (update(ScoreModel).where(ScoreModel.id == score_id).values({'name': name}))
 
-    if entry_to_be_updated.name != name:
-        update_data['name'] = name
-    if entry_to_be_updated.math_score != math_score:
-        update_data['math_score'] = math_score
-    if entry_to_be_updated.english_score != english_score:
-        update_data['english_score'] = english_score
-    if update_data:
-        query = (update(ScoreModel).where(cast("ColumnElement[bool]",entry_to_be_updated.id == score_id))
-                                   .values(update_data))
-        db.execute(query)
-        # TODO: Implements exception and rollback in case of error, apply ACID principles
-        db.commit()
+            if row.math_score != math_score:
+                query = (update(ScoreModel).where(ScoreModel.id == score_id).values({'math_score': math_score}))
 
-    updated_score = db.query(ScoreModel).filter_by(id=id).first()
-    return updated_score
+            if row.english_score != english_score:
+                query = (update(ScoreModel).where(ScoreModel.id == score_id).values({'english_score': english_score}))
+
+            db.execute(query)
+            db.commit()
+            session.commit()
+
+            return {"Message": "Updated successfully!"}
+
+        except Exception:
+            session.rollback()
+            # TODO: Handle the exception appropriately (log, return error message, etc.)
+            raise  # Re-raise the exception for now
